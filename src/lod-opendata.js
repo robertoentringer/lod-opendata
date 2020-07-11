76788920a40e696ff33b899413c5bcda764acdaa
Roberto Enringer <r.entringer@gmail.com>
2020-07-11 14:48:14 +0200

    run lint

--- src/lod-opendata.js
+++ src/lod-opendata.js
@@ -6,20 +6,20 @@ const request = (fields = "") =>
   new Promise((resolve, reject) => {
     ;(function req(endPoint) {
       https
-        .get(endPoint, { headers: { "X-Fields": fields } }, resp => {
+        .get(endPoint, { headers: { "X-Fields": fields } }, (resp) => {
           if (resp.statusCode >= 300 && resp.statusCode < 400 && resp.headers.location)
             return req(resp.headers.location)
           if (resp.statusCode !== 200)
             reject(`${resp.statusCode} : ${resp.statusMessage} ${endPoint}`)
           let body = ""
-          resp.on("data", data => (body += data))
+          resp.on("data", (data) => (body += data))
           resp.on("end", () => resolve(body))
         })
         .on("error", reject)
     })(endPoint)
   })
 
-module.exports = fields =>
+module.exports = (fields) =>
   request(fields)
-    .then(resp => JSON.parse(resp))
-    .catch(err => err)
+    .then((resp) => JSON.parse(resp))
+    .catch((err) => err)
